fbb26960e168df4b0ee58f1d3e3ede8789e49b7f
William <willlin1996@gmail.com>
2020-12-16 13:02:55 -0500

    mapping through devdata

--- src/components/DevCard.js
+++ src/components/DevCard.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import devData from '../components/DevData'
-import devCard from '../components/DevCard'
 
 class DevProject extends React.Component {
     render(){
         return(
-            <div className="DevProject">
+            <div className="DevCard">
                 
             </div>
         )
     }
 } 
 
-export default DevProject
+export default DevCard
